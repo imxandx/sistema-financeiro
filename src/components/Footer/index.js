3874989872c71574f7b13b88f430f5c3b967224c
Alexandre Delaboneta © <alexdelaboneta@gmail.com>
2024-08-29 21:46:32 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as C from "./styles";
+
+const Footer = () => {
+    return (
+        <C.Container>Alexandre Delaboneta &copy;</C.Container>
+    )
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    text-align: center;
+    color: #ccc;
+    font-size: 18px;
+    width: 100vw;
+    margin-top: 8%;
+`;
